0aee2b3889d6d847695809e78081120d0655b2e5
bobbybee <j104@rocketmail.com>
2014-10-16 13:46:15 -0400

    DistributedParamater, etc.

---
+++ bamboo/DistributedParameter.py
@@ -0,0 +1,10 @@
+class DistributedParamater:
+	def __init__(self, _type, name):
+		self._type = _type
+		self._name = name
+
+	def name(self):
+		return self._name
+
+	def type(self):
+		return self._type
---
+++ bamboo/DistributedType.py
@@ -0,0 +1,6 @@
+class DistributedType:
+	def __init__(self, strType):
+		self.strType = strType
+
+	def to_string(self):
+		return self.strType
