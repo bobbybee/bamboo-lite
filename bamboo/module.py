5386699732f11624e6cfd6b494724cd7b7bd1dce
bobbybee <j104@rocketmail.com>
2014-10-16 09:38:45 -0400

    Class of module inspection methods

--- bamboo/module.py
+++ bamboo/module.py
@@ -1,3 +1,12 @@
 class Module:
+	def __init__(self):
+		self.classes = []
+
 	def add_keyword(self, word):
 		print "Adding keyword "+word+"..."
+
+	def num_classes(self):
+		return self.classes.length
+
+	def get_class(self, num):
+		return self.classes[num]
